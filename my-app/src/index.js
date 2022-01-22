a9517e3087282c767bd29830efb0d9bddd50b829
David Shen <davidshen@asana.com>
2022-01-22 16:32:11 -0500

    k

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LobbyScreen from './lobby/LobbyScreen'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LobbyScreen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ my-app/src/lobby/LobbyScreen.js
@@ -0,0 +1,68 @@
+import './LobbyScreen.css';
+
+function LobbyScreen() {
+  const player_names = ['jax', 'daxter','ratchet','clank']
+  return (
+    <div>
+      <PlayerIconRow player_names={player_names}/>
+      <ReadyText ready_player_count='4'/>
+      <ReadyButton />
+      <NameInput />
+      <NameInputEnter />
+    </div>
+  )
+}
+
+function PlayerIconRow(props) {
+  const player_icons = props.player_names.map(name => <PlayerIcon player_name={name} key={name}/>)
+  return (
+    <div className='player-icon-row'>
+        {player_icons}
+    </div>
+  );
+}
+
+function PlayerIcon(props) {
+  return(
+    <div>
+        <span>👍 </span>
+        <span className='ready-text'>
+          {props.player_name}
+        </span>
+    </div>
+  );
+}
+
+function ReadyText(props) {
+  return (
+      <span className='ready-text'>
+        {props.ready_player_count}/8 Ready
+      </span>
+  );
+}
+
+function ReadyButton() {
+  return (
+    <div className='ready-btn'>
+      <button >Start Game</button>
+    </div>
+  )
+}
+
+function NameInput() {
+  return (
+    <div>
+      <input type='text' placeholder='Enter name'/>
+    </div>
+  )
+}
+  
+function NameInputEnter() {
+  return (
+    <div>
+      <button>Ready</button>
+    </div>
+  )
+}
+
+export default LobbyScreen;
