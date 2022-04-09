07e9f64c87970aa489292aea1ea6f3a0a509cd64
David Shen <davidshen@asana.com>
2022-04-09 16:17:01 -0400

    update game rules

--- Server/GameRules.py
+++ Server/GameRules.py
@@ -7,15 +7,20 @@ RESULTS_PHASE_TIME = 30
 CARDS = ["rhyme", "invert", "subvert", "swap", "pump", "dump", "troll"]
 
 PHRASES = [
-    "a cat has nine lives",
     "slow and steady wins the race",
     "a rolling stone gathers no moss",
-    "a penny saved is a penny earned",
-    "a picture is worth a thousand words",
+    "penny saved is a penny earned",
+    "every picture is worth a thousand words",
     "actions speak louder than words",
     "add insult to injury",
     "birds of a feather flock together",
     "every cloud has a silver lining",
-    "the early bird gets the worm",
-    "the elephant in the room"
+    "early bird gets the worm",
+    "the elephant in the room",
+    "fortune favors the bold",
+    "practice what you preach",
+    "best thing since sliced bread",
+    "cry over spilt milk",
+    "judge a book by its cover",
+    "break a leg",
 ]
